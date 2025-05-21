d96f21ac1876f00c96063b9a6ca58cffb7da6c14
Sara-Rubaya <sararubaya4800@gmail.com>
2025-05-21 22:44:11 +0600

    added more update

--- src/Components/UpdateGroup.jsx
+++ src/Components/UpdateGroup.jsx
@@ -7,7 +7,7 @@ const UpdateGroup = () => {
   const group = useLoaderData();
   const handleUpdateGroup = e =>{
     e.preventDefault();
-    const name = e.target.name.value;
+    const groupName = e.target.groupName.value;
     const category = e.target.category.value;
     const description = e.target.description.value;
     const meetingLocation = e.target.meetingLocation.value;
@@ -15,8 +15,21 @@ const UpdateGroup = () => {
     const startDate= e.target.startDate.value;
     const imageUrl= e.target.imageUrl.value;
    
+     const updatedGroup = {groupName, category, imageUrl,startDate, maxMembers, meetingLocation, description}
+    console.log(updatedGroup);
 
-    console.log(name, category, imageUrl,startDate, maxMembers, meetingLocation, description);
+    //update group info in the db
+    fetch(`http://localhost:3000/groups/${group._id}`,{
+      method: 'PUT',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(updatedGroup)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('after update', data)
+    })
   }
 
   const categories = [
@@ -34,7 +47,7 @@ const UpdateGroup = () => {
     <div className="max-w-2xl mx-auto px-6 py-10 bg-white rounded shadow">
       <h2 className="text-3xl font-bold text-center mb-8">Update Group</h2>
       <form onSubmit={handleUpdateGroup} className="space-y-4">
-        <input name="groupName" type="text" defaultValue={group.name} 
+        <input name="groupName" type="text" defaultValue={group.groupName} 
           placeholder="Group Name" className="input input-bordered w-full" required />
 
         <select name="category" defaultValue={group.category} 
@@ -59,8 +72,8 @@ const UpdateGroup = () => {
         <input name="imageUrl" type="text" defaultValue={group.imageUrl} 
           placeholder="Image URL" className="input input-bordered w-full" required />
 
-        <input type="text" Value={user?.displayName || 'Anonymous'} readOnly className="input input-bordered w-full bg-gray-100" />
-        <input type="email" Value={user?.email} readOnly className="input input-bordered w-full bg-gray-100" />
+        <input type="text" value={user?.displayName || 'Anonymous'} readOnly className="input input-bordered w-full bg-gray-100" />
+        <input type="email" value={user?.email} readOnly className="input input-bordered w-full bg-gray-100" />
 
         <button type="submit" className="btn btn-wide mx-auto bg-gray-700 text-white hover:bg-gray-800 ">
           Update Group
