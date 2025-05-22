1be4583d60cf5569d973f511fd2b6aee1581120f
Sara-Rubaya <sararubaya4800@gmail.com>
2025-05-22 12:39:19 +0600

    added typewriter

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { useTypewriter } from 'react-simple-typewriter';
 
 
 const Banner = () => {
+  const [text]=useTypewriter({
+    words:['Connect Through Hobbies!'],
+    loop:0
+  })
+
   return (
     <div className="hero min-h-[80vh] bg-base-200 rounded-lg shadow-md my-10">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -12,11 +18,12 @@ const Banner = () => {
           alt="Group Activities"
         />
         <div>
-          <h1 className="text-5xl font-bold text-gray-700">Connect Through Hobbies!</h1>
+          
+          <span className="text-5xl font-bold text-gray-700">{text}</span>
           <p className="py-6 text-gray-600">
             Join like-minded people, explore your passions, and create meaningful connections. Discover hobby groups, share your experiences, and grow together!
           </p>
-          <Link to="/allGroups">
+          <Link to="/*">
             <button className="btn btn-primary bg-gray-700 hover:bg-gray-800 text-white">
               Explore Groups
             </button>
